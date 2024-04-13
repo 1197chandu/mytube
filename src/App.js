05524edfcb0434fed253d3fa9fb59179562858f0
1197chandu <jadhavc1197@gmail.com>
2024-04-13 12:49:14 +0530

    Created empty components as per youtube UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Body from "./components/Body";
+import Header from "./components/Header";
 
 function App() {
-  return <div>MyTube</div>;
+  return (
+    <div>
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
